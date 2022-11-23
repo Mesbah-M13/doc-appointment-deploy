28b8dd2370667d8a8dabc996f58026598bdfb243
Mesbah-Himel <mesbah0010@gmail.com>
2022-11-23 14:42:40 +0600

    new commit

--- server.js
+++ server.js
@@ -7,22 +7,18 @@ const userRoute = require("./routes/userRoute");
 const adminRoute = require("./routes/adminRoute");
 const doctorRoute = require("./routes/doctorsRoute");
 const path = require("path");
-const {JWT_SECRET , MONGO_URL} = require('./config/keys')
 
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
-});
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
